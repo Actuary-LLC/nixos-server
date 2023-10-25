e1d270f922de5ff15254885ad0ef3c780b4e5814
AlexanderTroyScott <Alexander.Troy.Scott@gmail.com>
2023-10-25 08:46:43 -0500

    combined runner syntax

--- jupyter/github-runner.nix
+++ jupyter/github-runner.nix
@@ -11,33 +11,36 @@
       "nodejs-16.20.2"
     ];
   };
-
-    services.github-runner.jupyter = {
-      enable = true;
-      url = "https://github.com/Actuary-LLC/jupyter";
-      tokenFile = "/etc/nixos/jupyter.token";
-      replace = true;
-      serviceOverrides.ProtectHome = false;
-      extraLabels = ["jupyter"];
-      name = "jupyter";
-      user = "actuary";
-      workDir = "/tmp";
-      extraPackages = with pkgs; [ docker ];
-    };
-
-    services.github-runner.database = {
-      enable = true;
-      url = "https://github.com/Actuary-LLC/database";
-      tokenFile = "/etc/nixos/database.token";
-      replace = true;
-      serviceOverrides.ProtectHome = false;
-      extraLabels = ["database"];
-      name = "database";
-      user = "actuary";
-      workDir = "/tmp";
-      extraPackages = with pkgs; [ docker ];
-    };
-  
   services.github-runner.nodeRuntimes = ["node16"];
-
+  services.github-runner = {
+    enable = true;
+    runners = {
+      jupyter = {
+        repo = "Actuary-LLC/jupyter";
+        enable = true;
+        url = "https://github.com/Actuary-LLC/jupyter";
+        tokenFile = "/etc/nixos/jupyter.token";
+        replace = true;
+        serviceOverrides.ProtectHome = false;
+        extraLabels = ["jupyter"];
+        name = "jupyter";
+        user = "actuary";
+        workDir = "/tmp";
+        extraPackages = with pkgs; [ docker ];
+      };
+      database = {
+        repo = "Actuary-LLC/database";
+        enable = true;
+        url = "https://github.com/Actuary-LLC/database";
+        tokenFile = "/etc/nixos/database.token";
+        replace = true;
+        serviceOverrides.ProtectHome = false;
+        extraLabels = ["database"];
+        name = "database";
+        user = "actuary";
+        workDir = "/tmp";
+        extraPackages = with pkgs; [ docker ];
+      };
+    };
+  };
 }
